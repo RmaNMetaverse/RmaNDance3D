1042c745da7563c7f67885be38e45feacbf23d9d
RmaNMetaverse <106972081+RmaNMetaverse@users.noreply.github.com>
2025-06-11 15:38:03 +0330

    update build to docs

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: './', // This ensures assets are loaded correctly in GitHub Pages
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
     assetsDir: 'assets',
   },
 }); 
